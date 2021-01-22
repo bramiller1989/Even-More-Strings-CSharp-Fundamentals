ed92d7068a6bc4a0f2a8db2104b905fb87f4bb7b
bramiller1989 <bramiller1989@gmail.com>
2021-01-21 19:48:18 -0600

    use remove

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ namespace EvenMoreStrings
             builder.Append('-', 10);
 
             builder.Replace('-', '+');
+
+            builder.Remove(0, 10);
             Console.WriteLine(builder);
 
 
